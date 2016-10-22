fda22e720e52457f25cf566c67e545f9b8db3991
Gavin Stark <gavin@gstark.com>
2016-10-21 22:40:55 -0400

    Update

---
+++ atom/install.sh
@@ -0,0 +1,11 @@
+# star all the atom packages needed
+JSON=$(mktemp)
+apm stars --json > ${JSON}
+
+for package in linter file-icons pigments color-picker emmet git-plus language-babel merge-conflicts linter-eslint atom-ternjs linter-tidy linter-stylelint toggle-quotes standard-formatter html-entities atom-wrap-in-tag emmet-jsx-css-modules stylefmt; do
+  if ! grep -q "\"name\":\"${package}\"" ${JSON}; then
+    apm star ${package}
+  fi
+done
+
+apm stars --install
