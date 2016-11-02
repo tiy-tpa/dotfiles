848536559933919537ba840e3b4a1b21e143178d
Gavin Stark <gavin@gstark.com>
2016-11-02 11:53:36 -0400

    Cleans up the atom apm starring

--- atom-editor/install.sh
+++ atom-editor/install.sh
@@ -1,11 +1,13 @@
-# star all the atom packages needed
-JSON=$(mktemp)
-apm stars --json > ${JSON}
+if which apm; then
+  # star all the atom packages needed
+  JSON=$(mktemp)
+  apm stars --json > "${JSON}"
 
-for package in linter file-icons pigments color-picker emmet git-plus language-babel merge-conflicts linter-eslint atom-ternjs linter-tidy linter-stylelint toggle-quotes standard-formatter html-entities atom-wrap-in-tag emmet-jsx-css-modules stylefmt; do
-  if ! grep -q "\"name\":\"${package}\"" ${JSON}; then
-    apm star ${package}
-  fi
-done
+  for package in linter file-icons pigments color-picker emmet git-plus language-babel merge-conflicts linter-eslint linter-tidy linter-stylelint toggle-quotes standard-formatter html-entities atom-wrap-in-tag emmet-jsx-css-modules stylefmt; do
+    if ! grep -q "\"name\":\"${package}\"" ${JSON}; then
+      apm star "${package}"
+    fi
+  done
 
-# apm stars --install
+  # apm stars --install
+fi
